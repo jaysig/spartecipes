fd1aac465644aabed54ba978ffedb752c25724ac
amenotu <kyabut@gmail.com>
2015-12-02 14:08:39 -0800

    Added comments to scale.js

--- client/app/services/scale.js
+++ client/app/services/scale.js
@@ -1,9 +1,12 @@
 angular.module('ScaleFactory', [])
-  .factory('Scale', function() {
-
-    var formatAmount = function(amount) {
-      var wholeNum = parseInt(amount.toString().match(/\d+/));
-      var fraction = parseFloat(amount.toString().match(/\.\d+/));
+  .factory('Scale', ['Search', function (Search) {
+    /**
+     * Format scaled ingredient display quantity to show fractions for imperical
+     * @param {int} quantity [scaled Ingredient.Quantity]
+     */
+    var formatAmount = function (quantity) {
+      var wholeNum = parseInt(quantity.toString().match(/\d+/));
+      var fraction = parseFloat(quantity.toString().match(/\.\d+/));
 
       if(!wholeNum) {
         wholeNum = null;
@@ -25,20 +28,54 @@ angular.module('ScaleFactory', [])
 
     };
 
-    var scaleRecipe = function(scaleFactor, recipe) {
+    /**
+     * Scale recipe as a proportion of recipe's YieldNumber (servings),
+     * only changes display amount not actual ingredient quantity
+     * @param {int} scaleFactor [user-defined number of servings]
+     * @param {object} recipe [currently selected recipe]
+     */    
+
+    var scaleRecipe = function (scaleFactor, recipe) {
       var servings = recipe.YieldNumber;
       var ingredients = recipe.Ingredients;
-  		var scaled = [];
+      var scaledImperical = [];
+      var scaledMetric = [];
       _.each(ingredients, function(item, i) {
-        scaled.push(item.Quantity * (scaleFactor/servings));
+        //scaledImperical.push(item.Quantity * (scaleFactor/servings));
+        //scaledMetric.push(item.MetricQuantity * (scaleFactor/servings));
+
+        item.Quantity = item.Quantity * (scaleFactor/servings);
+        item.MetricQuantity = item.MetricQuantity * (scaleFactor/servings);
+
 
         if(item.DisplayQuantity) {
-          item.DisplayQuantity = formatAmount(scaled[i]);
+          item.DisplayQuantity = formatAmount(item.Quantity);
+          item.MetricDisplayQuantity = item.MetricQuantity;
         }
       });
+      console.log(ingredients);
     };
 
+    /**
+     * Scales the recipe and changes the quantities once the user 
+     * has decided on serving size
+     * @param {int} scaleFactor [user-defined number of servings]
+     * @param {object} recipe [currently selected recipe]
+     */  
+
+     // var restoreOriginalQuantity = function (recipe, ) {
+     //   Search.getSingleRecipe(recipe.RecipeID)
+     //   .then(function(recipe) {
+     //    var ingredients = recipe.Ingredients;
+     //      _.each(ingredients, function(item) {
+     //        item.Quantity
+     //      })
+     //   })
+     // }
+
+
   	return {
-  		scaleRecipe: scaleRecipe
+  		scaleRecipe: scaleRecipe,
   	};
-  });
+
+  }]);
