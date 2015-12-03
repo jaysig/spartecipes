347b81b6233cd3242575837b3a04ff2d29d605f4
amenotu <kyabut@gmail.com>
2015-12-02 17:00:14 -0800

    Removed console.logs and other comments, edited previous comments

--- client/app/services/scale.js
+++ client/app/services/scale.js
@@ -1,5 +1,6 @@
 angular.module('ScaleFactory', [])
-  .factory('Scale', ['Search', function (Search) {
+  .factory('Scale', function () {
+
     /**
      * Format scaled ingredient display quantity to show fractions for imperical
      * @param {int} quantity [scaled Ingredient.Quantity]
@@ -31,51 +32,28 @@ angular.module('ScaleFactory', [])
     /**
      * Scale recipe as a proportion of recipe's YieldNumber (servings),
      * only changes display amount not actual ingredient quantity
+     * need to use DisplayQuantity to get the right quantities to add to shopping list
      * @param {int} scaleFactor [user-defined number of servings]
      * @param {object} recipe [currently selected recipe]
      */    
-
     var scaleRecipe = function (scaleFactor, recipe) {
       var servings = recipe.YieldNumber;
       var ingredients = recipe.Ingredients;
       var scaledImperical = [];
       var scaledMetric = [];
       _.each(ingredients, function(item, i) {
-        //scaledImperical.push(item.Quantity * (scaleFactor/servings));
-        //scaledMetric.push(item.MetricQuantity * (scaleFactor/servings));
-
-        item.Quantity = item.Quantity * (scaleFactor/servings);
-        item.MetricQuantity = item.MetricQuantity * (scaleFactor/servings);
-
+        scaledImperical.push(item.Quantity * (scaleFactor/servings));
+        scaledMetric.push(item.MetricQuantity * (scaleFactor/servings));
 
         if(item.DisplayQuantity) {
-          item.DisplayQuantity = formatAmount(item.Quantity);
-          item.MetricDisplayQuantity = item.MetricQuantity;
-        }
+          item.DisplayQuantity = formatAmount(scaledImperical[i]);
+          item.MetricDisplayQuantity = scaledMetric[i];
+          }
       });
-      console.log(ingredients);
     };
-
-    /**
-     * Scales the recipe and changes the quantities once the user 
-     * has decided on serving size
-     * @param {int} scaleFactor [user-defined number of servings]
-     * @param {object} recipe [currently selected recipe]
-     */  
-
-     // var restoreOriginalQuantity = function (recipe, ) {
-     //   Search.getSingleRecipe(recipe.RecipeID)
-     //   .then(function(recipe) {
-     //    var ingredients = recipe.Ingredients;
-     //      _.each(ingredients, function(item) {
-     //        item.Quantity
-     //      })
-     //   })
-     // }
-
 
   	return {
   		scaleRecipe: scaleRecipe,
   	};
 
-  }]);
+  });
