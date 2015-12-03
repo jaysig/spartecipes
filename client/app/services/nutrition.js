7af8e2b31d1027da93cfd5773d4478b4128fe22d
Ben Pochily <ben.pochily@gmail.com>
2015-12-03 17:41:57 -0500

    implemented warnings when nutrition is incomplete

--- client/app/services/nutrition.js
+++ client/app/services/nutrition.js
@@ -7,11 +7,15 @@ angular.module('NutritionFactory', [])
         })
         .then(function(data) {
           data.data.nutritionTotals = {};
+          data.data.nutritionComplete = true;
           var nutrientStrings = ['Calories', 'Calories from fat', 'Total fat', 'Cholesterol', 'Sodium', 'Total carbohydrate', 'Protein'];
           for (var i = 0; i < nutrientStrings.length; i++) {
             data.data.nutritionTotals[nutrientStrings[i]] = 0;
             for (var j = 0; j < data.data.Ingredients.length; j++) {
               data.data.nutritionTotals[nutrientStrings[i]] += data.data.Ingredients[j].Nutrition[nutrientStrings[i]];
+              if (!data.data.Ingredients[j].Nutrition.complete) {
+                data.data.nutritionComplete = false;
+              }
             }
             data.data.nutritionTotals[nutrientStrings[i]] = ~~data.data.nutritionTotals[nutrientStrings[i]];
           }
