40fa4fbf5b1be2e95b0cfd6bf8dcb5027974c287
Ben Pochily <ben.pochily@gmail.com>
2015-12-03 19:38:56 -0500

    fix incorrect reference

--- client/app/shoppingList/shoppingList.js
+++ client/app/shoppingList/shoppingList.js
@@ -31,7 +31,7 @@ angular.module('recipes')
      */
     $scope.removeItem = function(id) {
       ShoppingList.removeFromList(id);
-      getList();     
+      $scope.list = ShoppingList.getList();     
     };
 
     /**
