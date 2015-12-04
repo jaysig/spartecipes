18d7a8e2c9aeaa3b094c71af4c83b42e42188447
Ben Pochily <ben.pochily@gmail.com>
2015-12-03 19:52:27 -0500

    removed final problem

--- client/app/shoppingList/shoppingList.js
+++ client/app/shoppingList/shoppingList.js
@@ -21,9 +21,6 @@ angular.module('recipes')
 
     getUserList();
 
-    /*$rootScope.$on('userAction', function() {
-      getUserList();
-    });*/
     /**
      * Remove a recipe from the list and refreshes scope list with
      * recipes and ingredients
